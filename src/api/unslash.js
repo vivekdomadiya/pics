6f58e77a47ef883c1cd5c5c39144c04c148a714e
VivekDomadia <domadia.vivek.2@ldce.ac.in>
2020-07-14 11:24:03 +0530

    Updates

--- src/api/unslash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com/",
-  headers: {
-    Authorization: "Client-ID J-Gq9L2pa4yT17cN0ZknmptH9ILo8zOTv4P0Ik-2Frg",
-  },
-});
